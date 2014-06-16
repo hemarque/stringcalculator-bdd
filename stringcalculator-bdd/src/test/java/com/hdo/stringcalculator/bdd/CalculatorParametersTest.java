a67b975062f5c0358982de554eabb0fe921439cb
Helder Enrique De Oliveira Garrido <helderenrique.deoliveira@atsistemas.com>
2014-06-16 13:33:12 +0200

    Plantilla HTML de Concordion y Fixture (Test) asociado para la ejecución de criterios de aceptación

---
+++ stringcalculator-bdd/src/test/java/com/hdo/stringcalculator/bdd/CalculatorParametersTest.java
@@ -0,0 +1,13 @@
+package com.hdo.stringcalculator.bdd;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class CalculatorParametersTest {
+
+    public int add(String numbers) {
+        return 0;
+    }
+
+}
