c9b3365a6bbc0d932266f5b45668313b92dcbbe4
Helder Enrique De Oliveira Garrido <helderenrique.deoliveira@atsistemas.com>
2014-06-16 15:04:52 +0200

    Refactorización: extraer a clase de dominio

--- stringcalculator-bdd/src/test/java/com/hdo/stringcalculator/bdd/CalculatorParametersTest.java
+++ stringcalculator-bdd/src/test/java/com/hdo/stringcalculator/bdd/CalculatorParametersTest.java
@@ -3,24 +3,14 @@ package com.hdo.stringcalculator.bdd;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
+import com.hdo.stringcalculator.StringCalculator;
+
 @RunWith(ConcordionRunner.class)
 public class CalculatorParametersTest {
 
 	public int add(String numbers) {
-		if (numbers.length() == 0)
-			return 0;
-		return add(numbers.split(","));
-	}
-
-	private int add(String[] numbersArray) {
-		return numbersArray.length == 1 ? Integer.parseInt(numbersArray[0])
-				: addFirstTwo(numbersArray);
-	}
-
-	private int addFirstTwo(String[] numbersArray) {
-		int first = Integer.parseInt(numbersArray[0]);
-		int second = Integer.parseInt(numbersArray[1]);
-		return first + second;
+		StringCalculator calculator = new StringCalculator();
+		return calculator.add(numbers);
 	}
 
 }
