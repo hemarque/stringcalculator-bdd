f9bb015dc64013df953028fd5d3ab114e8047e1b
Helder Enrique De Oliveira Garrido <helderenrique.deoliveira@atsistemas.com>
2014-06-16 16:58:01 +0200

    Segundo paso de la kata, cálculo de suma de un conjunto de números de tamaño desconocido

--- stringcalculator-bdd/src/main/java/com/hdo/stringcalculator/StringCalculator.java
+++ stringcalculator-bdd/src/main/java/com/hdo/stringcalculator/StringCalculator.java
@@ -3,20 +3,16 @@ package com.hdo.stringcalculator;
 public class StringCalculator {
 
 	public int add(String numbers) {
-		if (numbers.length() == 0)
-			return 0;
 		return add(numbers.split(","));
 	}
 
 	private int add(String[] numbersArray) {
-		return numbersArray.length == 1 ? Integer.parseInt(numbersArray[0])
-				: addFirstTwo(numbersArray);
-	}
-
-	private int addFirstTwo(String[] numbersArray) {
-		int first = Integer.parseInt(numbersArray[0]);
-		int second = Integer.parseInt(numbersArray[1]);
-		return first + second;
+		int result = 0;
+		for (String number : numbersArray) {
+			if (!number.isEmpty())
+				result += Integer.parseInt(number);
+		}
+		return result;
 	}
 
 }
