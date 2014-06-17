44f093a686d266ebc8cb34bf43e82f39e781d1b1
Helder Enrique De Oliveira Garrido <helderenrique.deoliveira@atsistemas.com>
2014-06-17 10:22:03 +0200

    Incorporar nuevos delimitadores en la sintaxis

--- stringcalculator-bdd/src/main/java/com/hdo/stringcalculator/StringCalculator.java
+++ stringcalculator-bdd/src/main/java/com/hdo/stringcalculator/StringCalculator.java
@@ -1,10 +1,25 @@
 package com.hdo.stringcalculator;
 
-
 public class StringCalculator {
 
 	public int add(String numbers) {
-		return add(numbers.split("\\\\n|,"));
+		String delimiters = getDelimiters(numbers);
+		numbers = cleanNumbersFormat(numbers, delimiters);
+		return add(numbers.split(delimiters));
+	}
+
+	private String cleanNumbersFormat(String numbers, String delimiters) {
+		return numbers.startsWith("//") ? numbers.substring(numbers
+				.indexOf("\\n") + 2) : numbers;
+	}
+
+	private String getDelimiters(String numbers) {
+		return numbers.startsWith("//") ? calculateDelimiters(numbers)
+				: "\\\\n|,";
+	}
+
+	private String calculateDelimiters(String numbers) {
+		return numbers.substring(2, numbers.indexOf("\\n"));
 	}
 
 	private int add(String[] numbersArray) {
